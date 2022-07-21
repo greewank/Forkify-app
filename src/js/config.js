bec2c4bc0d118f24d14da9e592cfaaf5e4e606a4
greewank <gkc1s@semo.edu>
2022-07-21 12:33:46 -0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// Using variables that are going to be constant and be reused throughout the project.
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eb0d286b-2c27-4293-9543-0a6049f1269d';
+export const MODAL_CLOSE_SEC = 2.5;
